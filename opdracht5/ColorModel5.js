1dd928a51e61a3c95b8db55121ca1a8bfc31cbd8
HjalmarSnoep <github@snoep.at>
2017-05-22 06:54:06 +0200

    Add files via upload

---
+++ opdracht5/ColorModel5.js
@@ -0,0 +1,38 @@
+function ColorModel(r,g,b)
+{
+		
+	// make sure you can define your object a number of ways
+	// leaving rgb out, gives you black.
+	if(typeof(r)=="undefined")r=Math.floor(Math.random()*256);
+	if(typeof(g)=="undefined")g=Math.floor(Math.random()*256);
+	if(typeof(b)=="undefined")b=Math.floor(Math.random()*256);
+
+	// copy the args to the object we will return.
+	this.r=r;
+	this.g=g;
+	this.b=b;
+
+	// maakt de kleur in het model donkerder
+	this.darkenColor=function(f1)
+	{
+		if(typeof(f1)=="undefined")f1=0.3;
+		var f2=1-f1;
+		this.r=Math.floor(this.r*f2+f1*0);
+		this.g=Math.floor(this.g*f2+f1*0);
+		this.b=Math.floor(this.b*f2+f1*0);
+	}
+	
+	// bereken een grijskleur (tussen 0 en 255) op basis van onze r,g,b
+	this.getGrayValue=function()
+	{
+		var grey=(this.r+this.g+this.b)/3; // simple grey!
+		return grey;
+	}
+	
+	// maak een string als "rgb(255,255,255)" van onze r,g,b
+	this.toString=function()
+	{
+		return "rgb("+this.r+","+this.g+","+this.b+")";
+	}
+	
+}
---
+++ opdracht5/ColorView5.js
@@ -0,0 +1,32 @@
+function ColorView(col)
+{
+	this.dom=document.createElement("div");
+	
+	// gebruikt cssText om meerdere eigenschappen in een keer te zetten!
+	this.dom.style.cssText = "width: 150px; height: 150px; border: 1px solid black; display: inline-block; margin: 5px;"; 
+
+	this.update=function(col)
+	{
+		this.dom.style.backgroundColor=col.toString();
+		this.dom.innerHTML=col.toString();
+		// kijk of je witte of zwarte text moet gebruiken.
+		var greyValue=col.getGrayValue();
+		if(greyValue<128)
+		{
+			this.dom.style.color="#fff";
+		}else{		
+			this.dom.style.color="#000";
+		}
+	};
+	
+	this.update(col);
+	
+	// maak het zichtbaar.
+	document.body.appendChild(this.dom);
+	
+	this.addCallback=function(callback)
+	{
+		this.callback=callback; // keep the (1 for now) subscriberCallback
+		console.log("A subscriber was added to the view."+this.subscriber);
+	};
+}
